2e01937715175ea4c9c7aed69fd3d4a39c3efd0e
rafasnarf <snarfelcv@gmail.com>
2019-10-29 23:19:37 -0300

    Backend em desenvolvimento 80%

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '67cf6d3adce925',
+    pass: '7baddce2f14455',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gorbarber.com>',
+  },
+};
--- src/database/index.js
+++ src/database/index.js
@@ -11,6 +11,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -22,9 +23,14 @@ class Database {
   }
 
   mongo() {
-    this.mongoConnecttion = mongoose.connect(
-      'mondodb://localhost:27017/gobarber',
-      { useNewUrlParser: true, useFindAndModify: true }
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+      }
     );
   }
 }
