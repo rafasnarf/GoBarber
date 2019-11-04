36bb58de5c5f35b91cf8086dab6c53a981df1acd
rafasnarf <snarfelcv@gmail.com>
2019-11-04 20:19:48 -0300

    Backend Finalizado

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '67cf6d3adce925',
-    pass: '7baddce2f14455',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gorbarber.com>',
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://05d893ea1d5745d5a73697583b97a948@sentry.io/1807746',
+};
